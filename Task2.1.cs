570c4ebf24de25f8401b44f128c40f9ce1545de5
Kadzik <kostakalenik2002.09k@mail.com>
2024-10-27 17:01:49 +0100

    all changes done

--- Task2.1.cs
+++ Task2.1.cs
@@ -1,56 +1,5 @@
 ﻿using System;
 
-public class Point3D
-{
-    private int[] coordinates = new int[3]; // X, Y, Z
-    private double mass;
-
-    public int X
-    {
-        get { return coordinates[0]; }
-        set { coordinates[0] = value; }
-    }
-
-    public int Y
-    {
-        get { return coordinates[1]; }
-        set { coordinates[1] = value; }
-    }
-
-    public int Z
-    {
-        get { return coordinates[2]; }
-        set { coordinates[2] = value; }
-    }
-
-    public double Mass
-    {
-        get { return mass; }
-        set { mass = value < 0 ? 0 : value; }
-    }
-
-    public Point3D(int x, int y, int z, double mass)
-    {
-        X = x;
-        Y = y;
-        Z = z;
-        Mass = mass;
-    }
-
-    public bool IsZero()
-    {
-        return X == 0 && Y == 0 && Z == 0;
-    }
-
-    public double DistanceTo(Point3D other)
-    {
-        int dx = X - other.X;
-        int dy = Y - other.Y;
-        int dz = Z - other.Z;
-        return Math.Sqrt(dx * dx + dy * dy + dz * dz);
-    }
-}
-
 public class Program
 {
     public static void Main()
